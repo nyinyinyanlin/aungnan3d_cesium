8fdad54b753e3dce3d3c64202a3c80a47f3460dd
Nyi Nyi Nyan Lin <nyinyinyanlin.mm@gmail.com>
2024-03-30 02:15:14 +0000

    Added POIs and POI images.

--- app.js
+++ app.js
@@ -27,8 +27,20 @@ app.get('/', (req, res) => {
     res.render('main', {layout : 'index'});
 })
 
-app.get('/geojson', async (req, res) => {
+app.get('/geojson/buildings', async (req, res) => {
     const result = await pool.query("SELECT json_build_object('type','Feature','id',id,'geometry',ST_AsGeoJSON(geom)::json,'properties',json_build_object('plate_id', plate_id,'type', type, 'monument_name', monument_name, 'era', era, 'description', description, 'donor', donor, 'image', image, 'dsm_height', dsm_height, 'delta_height', delta_height)) FROM \"public\".\"primary_buildings_final\";") // LIMIT 10
+    let geojson = {
+        type: "FeatureCollection",
+        features: []
+    } 
+    result.rows.forEach((row,i) => {
+        geojson.features.push(row.json_build_object)
+    })
+    res.json(geojson)
+})
+
+app.get('/geojson/poi', async (req, res) => {
+    const result = await pool.query("SELECT json_build_object('type','Feature','id',id,'geometry',ST_AsGeoJSON(geom)::json,'properties',json_build_object('plate_id', plate_id, 'poi_id', poi_id, 'description', description, 'image', photo, 'dsm_height', dsm_height)) FROM \"public\".\"poi_final\";") // LIMIT 10
     let geojson = {
         type: "FeatureCollection",
         features: []
